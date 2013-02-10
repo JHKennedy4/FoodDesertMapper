c2a4de1bbcf2ef6cdbe06cc56497bb9c47ef0770
John Kennedy <jhkennedy4@gmail.com>
2013-02-09 21:08:25 -0500

    heropush

--- public/javascripts/mymaps.js
+++ public/javascripts/mymaps.js
@@ -1,14 +1,27 @@
 var map;
 dojo.require("esri.map");
 
+function zoomToLocation(location) {
+    var pt = esri.geometry.geographicToWebMercator(new esri.geometry.Point(
+                 location.coords.longitude, location.coords.latitude));
+    map.centerAndZoom(pt, 16);
+}
+
+function locationError(error) {
+
+}
+
 function init() {
     //Rochester, NY: 43.1547, -77.6158
     map = new esri.Map("mapDiv", {
-	// change to user location
+        // change to user location
         center: [-77.6068, 43.1562],
         zoom: 13,
         basemap: "streets"
     });
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(zoomToLocation, locationError);
+    }
 }
 
 dojo.addOnLoad(init);
