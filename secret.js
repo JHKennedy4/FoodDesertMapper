c71cb89bd8b6e02c1f4e797dda3d03a23bc2a234
John Kennedy <jhkennedy4@gmail.com>
2013-02-18 00:54:43 -0500

    Added CartoDB library

---
+++ secret.js
@@ -0,0 +1,7 @@
+module.exports = {
+    USER: 'jhk',
+    password: 'donotb3Bored',
+    API_KEY: 'c3c310cb4bf016cd634e4df3d0a88b82826a4fbb',
+    CONSUMER_KEY: 'lZQ0bZuxqqLSQzHMZZ4tGgYwiD40TBGZL7cfv0a2',
+    CONSUMER_SECRET: '58Cr9AqIel5aOwNkerq8lnFqCAKOrKimF2Zawb93'
+};
