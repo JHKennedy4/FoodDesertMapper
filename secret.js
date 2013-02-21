d09e19e699086855d055940c4ca895c24dc709d7
John Kennedy <jhkennedy4@gmail.com>
2013-02-20 20:15:25 -0500

    getting rid of the secrets for publication of code

--- secret.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    USER: 'jhk',
-    password: 'donotb3Bored',
-    API_KEY: 'c3c310cb4bf016cd634e4df3d0a88b82826a4fbb',
-    CONSUMER_KEY: 'lZQ0bZuxqqLSQzHMZZ4tGgYwiD40TBGZL7cfv0a2',
-    CONSUMER_SECRET: '58Cr9AqIel5aOwNkerq8lnFqCAKOrKimF2Zawb93'
-};
