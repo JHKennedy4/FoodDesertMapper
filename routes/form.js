7d15583a659f9a0e86d885422f98cc3cee107777
John Kennedy <JHKennedy4@gmail.com>
2013-04-15 12:28:24 -0400

    switches to ejs

--- routes/form.js
+++ routes/form.js
@@ -4,6 +4,6 @@
  */
 
 exports.form = function (req, res) {
-    // got rid of jade, need to fix
-    res.render('form', { title: 'Rochester Food Desert Mapper' });
+    // got rid of jade, need to fix for ejs
+    res.render('form', { title: 'EJS Works!' });
 };
