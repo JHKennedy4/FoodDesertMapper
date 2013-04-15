a2833462da3f8f69b3d74e987416767ef22ad672
John Kennedy <JHKennedy4@gmail.com>
2013-04-15 16:38:08 -0400

    fixes data returned from cartodb

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.get('/edit/:id', function (req, res) {
     var id = req.params.id;
     client.query("select * from monroecountysnap where cartodb_id = " + id,
             function (err, data) {
-                res.render('form', { store: data.rows[0] });
+                res.render('form', { store: data });
             });
 }); //form.form);
 
