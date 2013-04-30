05ef2659ae073d9ece039b30267cb431404ecbcc
John Kennedy <JHKennedy4@gmail.com>
2013-04-30 00:15:25 -0400

    moved vars

--- public/javascripts/foodmap.js
+++ public/javascripts/foodmap.js
@@ -3,24 +3,35 @@ var baseMap =  new L.StamenTileLayer("toner");
 function main() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
+            var userMarker,
+                layerUrl,
+                layerOptions;
+
+            // create a new map at your location
             F.map = new L.map('map', {
                 center: new L.LatLng(position.coords.latitude, position.coords.longitude),
                 zoom: 16
             });
 
+            // add the baseMap to the layer
             F.map.addLayer(baseMap);
 
-            var userMarker = L.AwesomeMarkers.icon({
+            // options for the AwesomeMarker
+            userMarker = L.AwesomeMarkers.icon({
                 icon: 'icon-user',
                 color: 'red'
             });
 
+            // create an AwesomeMarker at the user location
             L.marker([position.coords.latitude, position.coords.longitude], {icon: userMarker}).bindPopup('<p>You are here</p>').addTo(F.map);
 
-            var layerUrl = 'http://jhk.cartodb.com/api/v1/viz/snap/viz.json',
-                layerOptions = {
-                    query: 'select cartodb_id, store_name, the_geom_webmercator from {{table_name}}',
-                };
+            // the layer url and options from CartoDB
+            // not sure if the query in layerOptions is actually necessary
+            layerUrl = 'http://jhk.cartodb.com/api/v1/viz/snap/viz.json';
+            layerOptions = {
+                query: 'select cartodb_id, store_name, the_geom_webmercator from {{table_name}}',
+            };
+
             cartodb.createLayer(F.map, layerUrl, layerOptions)
                 .on('done', function (layer) {
                     console.log("adding layer");
