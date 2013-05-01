90451c9750f32de1d7b1c612151d9083cae47060
Adam Blaine <amb8805@rit.edu>
2013-04-30 23:27:12 -0400

    double the ered isze

--- public/javascripts/foodmap.js
+++ public/javascripts/foodmap.js
@@ -19,7 +19,7 @@ function main() {
             // options for the AwesomeMarker
             userMarker = L.icon({
                 iconUrl: '/bootstrap/here.svg',
-                iconSize:     [38, 95], // size of the icon
+                iconSize:     [76, 190], // size of the icon
                 iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
                 popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
             });
